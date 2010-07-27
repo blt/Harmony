7a1b4084003b67d2d91b2a315e0f952c87531e66
Cameron Kidd <subtlegiant@host-247-114.pubnet.pdx.edu>
2010-07-26 20:57:39 -0700

    added rings: final prototype code

--- client/src/planet.py
+++ client/src/planet.py
@@ -3,19 +3,34 @@ import math
 import operator
 from pygame import image
 
-class Planet(object):
-    #image_pth = os.path.join(os.environ["HARMONY_MEDIA_DIR"],
-    #                         "planet.png")
-    image_pth = os.path.join('../media', 'planet.png')
+class Planet(pygame.sprite.Sprite):
+    image_pth = os.path.join(os.environ["HARMONY_MEDIA_DIR"],
+                             "planet.png")
+   # image_pth = os.path.join('../media', 'planet.png')
+    image = None
+    rect  = None
+    pid = 0
+    sid = 0
+    radius = 0
+    vertx = 0
+    verty = 0
+    x = 0
+    y = 0
 
-    def __init__(self, radius, startpos):
+    # attrs = (radius, startpos, sid)
+    def __init__(self, attrs, pid, angle):
+        pygame.sprite.Sprite.__init__(self)
         self.image = image.load(self.image_pth).convert();
-        self.planet = self.image.get_rect(center = startpos)
-        self.radius = radius 
-        self.vertx = 500
-        self.verty = 350
+        self.radius = attrs[0]
+        self.sid = attrs[2]
+        self.pid = pid
+        startpos = attrs[1] 
+        self.vertx = startpos[0]
+        self.rect = self.image.get_rect(center = startpos)
+        self.verty = startpos[1]
         self.x = 0
         self.y = 0
+        self.rotate(angle)
 
     def get_image(self):
         return self.image
